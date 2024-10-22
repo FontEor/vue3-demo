ab62ae4c46ce6d317aaa8f1b6cdb2a4ba112adb0
FontEor <583139315@qq.com>
2024-10-22 17:19:08 +0800

    feat: 项目初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <canvas id="QRCode_header" style="width: 280px; height: 280px"></canvas>
   </div>
 </template>
 
 <script>
+import QRCode from "qrcode"; //引入生成二维码插件
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  props: [""],
+  data() {
+    return {
+      qrUrl: location.href,
+    };
+  },
+  watch: {},
+  mounted() {
+    this.getQRCode();
+  },
+  created() {},
+  methods: {
+    getQRCode() {
+      //生成的二维码为URL地址js
+      this.qrUrl =
+        "http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg"; //"https://www.baidu.com/";
+      let opts = {
+        errorCorrectionLevel: "H", //容错级别
+        type: "image/png", //生成的二维码类型
+        quality: 0.3, //二维码质量
+        margin: 0, //二维码留白边距
+        width: 280, //宽
+        height: 280, //高
+        text: "http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg", //二维码内容https://www.baidu.com/
+        color: {
+          dark: "#333333", //前景色
+          light: "#fff", //背景色
+        },
+      };
+
+      let msg = document.getElementById("QRCode_header");
+      // 将获取到的数据（val）画到msg（canvas）上
+      QRCode.toCanvas(msg, this.qrUrl, opts, function (error) {
+        if (error) {
+          console.log("二维码加载失败", error);
+          this.$message.error("二维码加载失败");
+        }
+      });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
